8a4aac87977db3d593a80071d65eb5d09d91a9c7
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-11-07 16:13:51 +0100

    Sistema de idioma mejorado, más mantenible y legible

--- src/i18n/index.js
+++
@@ -1,5 +0,0 @@
-import es from './es.json';
-import en from './en.json';
-import nl from './nl.json';
-
-export const translations = { es, en, nl };
---
+++ src/i18n/ui.ts
@@ -0,0 +1,87 @@
+export const ui = {
+    es: {
+        'header.home': 'Inicio',
+        'header.company': 'La empresa',
+        'header.howitworks': 'Cómo funciona',
+        'header.prices': 'Tarifas',
+        'header.booking': 'Reservar',
+        'header.extraservices': 'Servicios adicionales',
+        'header.location': 'Localización',
+
+        'auth.login': 'Iniciar sesión',
+        'auth.register': 'Registrarse',
+        'auth.logout': 'Cerrar sesión',
+        'auth.myprofile': 'Mi perfil',
+
+        "carousel.title01": "Plazas al aire libre",
+        "carousel.button01": "Más información",
+        "carousel.title02": "Atención personalizada",
+        "carousel.text02": "Servicio de recogida y entrega del vehículo en la misma terminal",
+        "carousel.button02": "Reservar ahora",
+        "carousel.title03": "Plazas cubiertas",
+        "carousel.button03": "Más información",
+        "carousel.title04": "Parking",
+        "carousel.text04": "Estación de tren de Alicante",
+        "carousel.button04": "Llame ahora",
+        "carousel.title05": "Parking",
+        "carousel.text05": "Aeropuerto Alicante-Elche",
+        "carousel.button05": "Contáctenos"
+    },
+    en: {
+        'header.home': 'Home',
+        'header.company': 'Company',
+        'header.howitworks': 'How it works',
+        'header.prices': 'Prices',
+        'header.booking': 'Booking',
+        'header.extraservices': 'Extra services',
+        'header.location': 'Location',
+
+        'auth.login': 'Login',
+        'auth.register': 'Register',
+        'auth.logout': 'Logout',
+        'auth.myprofile': 'My Profile',
+
+        "carousel.title01": "Outdoor parking",
+        "carousel.button01": "More info",
+        "carousel.title02": "Personalized attention",
+        "carousel.text02": "Collection and delivery service of the vehicle at the same terminal",
+        "carousel.button02": "Book now",
+        "carousel.title03": "Indoor parking",
+        "carousel.button03": "More info",
+        "carousel.title04": "Parking",
+        "carousel.text04": "Alicante Train Station",
+        "carousel.button04": "Call now",
+        "carousel.title05": "Parking",
+        "carousel.text05": "Alicante-Elche Airport",
+        "carousel.button05": "Contact us"
+    },
+    nl: {
+        'header.company': 'Bedrijf',
+        'header.howitworks': 'Hoe het werkt',
+        'header.prices': 'Prijzen',
+        'header.booking': 'Reservering',
+        'header.extraservices': 'Extra diensten',
+        'header.location': 'Locatie',
+
+        'auth.login': 'Inloggen',
+        'auth.register': 'Registreren',
+        'auth.logout': 'Uitloggen',
+        'auth.myprofile': 'Mijn profiel',
+
+        "carousel.title01": "Openlucht parkeerplaatsen",
+        "carousel.button01": "Meer informatie",
+        "carousel.title02": "Persoonlijke aandacht",
+        "carousel.text02": "Ophaal- en bezorgservice van het voertuig op dezelfde terminal",
+        "carousel.button02": "Boek nu",
+        "carousel.title03": "Overdekte parkeerplaatsen",
+        "carousel.button03": "Meer informatie",
+        "carousel.title04": "Parking",
+        "carousel.text04": "Treinstation Alicante",
+        "carousel.button04": "Bel nu",
+        "carousel.title05": "Parking",
+        "carousel.text05": "Luchthaven Alicante-Elche",
+        "carousel.button05": "Neem contact met ons op"
+    },
+} as const;
+
+export const defaultLang = 'es';
