d66fb51fd1e5abacbe44567502b41c6b38a60b84
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-11-07 18:11:22 +0100

    La empresa y como funciona terminados

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,9 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+    nl: 'Nederlands'
+};
+
 export const ui = {
     es: {
         'header.home': 'Inicio',
@@ -13,6 +19,15 @@ export const ui = {
         'auth.logout': 'Cerrar sesión',
         'auth.myprofile': 'Mi perfil',
 
+        'footer.airport': 'Aeropuerto',
+        'footer.office': 'Oficina',
+        'footer.partners': 'Colaboradores',
+        'footer.secure': 'Protegidos y asegurados por',
+        'footer.longterm': 'Parking de larga estancia',
+        'footer.legalnotices': 'Avisos legales',
+        'footer.cookiespolicy': 'Política de cookies',
+        'footer.privacypolicy': 'Política de privacidad',
+        
         "carousel.title01": "Plazas al aire libre",
         "carousel.button01": "Más información",
         "carousel.title02": "Atención personalizada",
@@ -35,12 +50,21 @@ export const ui = {
         'header.booking': 'Booking',
         'header.extraservices': 'Extra services',
         'header.location': 'Location',
-
+        
         'auth.login': 'Login',
         'auth.register': 'Register',
         'auth.logout': 'Logout',
         'auth.myprofile': 'My Profile',
-
+        
+        'footer.airport': 'Airport',
+        'footer.office': 'Office',
+        'footer.partners': 'Partners',
+        'footer.secure': 'Secured and insured by',
+        'footer.longterm': 'Long-term parking',
+        'footer.legalnotices': 'Legal notices',
+        'footer.cookiespolicy': 'Cookies policy',
+        'footer.privacypolicy': 'Privacy policy',
+        
         "carousel.title01": "Outdoor parking",
         "carousel.button01": "More info",
         "carousel.title02": "Personalized attention",
@@ -62,12 +86,21 @@ export const ui = {
         'header.booking': 'Reservering',
         'header.extraservices': 'Extra diensten',
         'header.location': 'Locatie',
-
+        
         'auth.login': 'Inloggen',
         'auth.register': 'Registreren',
         'auth.logout': 'Uitloggen',
         'auth.myprofile': 'Mijn profiel',
-
+        
+        'footer.airport': 'Luchthaven',
+        'footer.office': 'Kantoor',
+        'footer.partners': 'Partners',
+        'footer.secure': 'Beveiligd en verzekerd door',
+        'footer.longterm': 'Lang parkeren',
+        'footer.legalnotices': 'Juridische kennisgevingen',
+        'footer.cookiespolicy': 'Cookiebeleid',
+        'footer.privacypolicy': 'Privacybeleid',
+        
         "carousel.title01": "Openlucht parkeerplaatsen",
         "carousel.button01": "Meer informatie",
         "carousel.title02": "Persoonlijke aandacht",
